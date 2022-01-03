e8c852b6089ae90aeaa0690c5c998a83613fd197
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2022-01-03 11:20:13 +0530

    Update guestbook.js
    API updated

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://52cb033b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
